b061363d884962da6d32d099d198d35a9467f2d1
nityakodali <124936302+nityakodali@users.noreply.github.com>
2023-03-22 21:12:33 -0400

    Created the encoder

---
+++ encoder.py
@@ -0,0 +1,36 @@
+def encoder(x):
+    password = ""
+    for char in x:
+        digit = int(char) + 10
+        digit = (digit + 3) % 10
+        password += str(digit)
+    return password
+
+
+def decoder(x):
+    ##
+
+
+def main():
+    password = ""
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            password = input("Please enter you password to encode: ")
+            password = encoder(password)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            print(f"The encoded password is {password}, and the original password is {decoder(password)}.")
+        elif option == 3:
+            break
+
+
+if __name__ == "__main__":
+    main()
